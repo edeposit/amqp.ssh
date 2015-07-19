ddda87338d177cd567390e0498957347f15787bc
Jan Stavěl <stavel.jan@gmail.com>
2015-07-19 16:42:17 +0200

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to edeposit.amqp.ssh
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/edeposit/amqp/ssh_test.clj
@@ -0,0 +1,7 @@
+(ns edeposit.amqp.ssh-test
+  (:require [clojure.test :refer :all]
+            [edeposit.amqp.ssh :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
